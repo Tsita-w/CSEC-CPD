076f2cbb8f30afd90e3003c47964118ad8c26b45
Tsita-w <t687959@gmail.com>
2025-02-05 20:09:15 +0300

    Add files via upload

---
+++ A-Magnets.py
@@ -0,0 +1,11 @@
+def count_groups(n, magnets):
+    groups = 1
+
+    for i in range(1, n):
+        if magnets[i] != magnets[i-1]:
+            groups += 1
+    return groups
+
+n = int(input())
+magnets = [input().strip() for _ in range(n)]
+print(count_groups(n, magnets))
