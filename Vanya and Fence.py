b781951665dcbfd7a971b9af62c0bb4c4e14f326
Tsita-w <t687959@gmail.com>
2025-02-05 14:31:02 +0300

    Update Vanya and Fence.py

--- Vanya and Fence.py
+++ Vanya and Fence.py
@@ -1,18 +1,14 @@
 
-n = int(input("Enter the number of friends: "))
-h = int(input("Enter the height of the fence: "))
+n, h = map(int, input().split())  
+a = list(map(int, input().split()))  
 
-heights = []
-for i in range(n):
-    height = int(input(f"Enter the height of friend {i+1}: "))
-    heights.append(height)
 
 total_width = 0
 
-for height in heights:
-    if height <= h:
-        total_width += 1
+for height in a:
+    if height > h:
+        total_width += 2  
     else:
-        total_width += 2
-        
-print( total_width)
+        total_width += 1  
+
+print(total_width)
