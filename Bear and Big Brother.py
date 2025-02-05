ac8ceef2aac770dbb1070bd8af61bf311f8dea22
Tsita-w <t687959@gmail.com>
2025-02-05 14:36:15 +0300

    Update Bear and Big Brother.py

--- Bear and Big Brother.py
+++ Bear and Big Brother.py
@@ -1,11 +1,7 @@
-a = int(input("Enter Limak's weight: "))
-b = int(input("Enter Bob's weight: "))
-
+a, b = map(int, input().split())
 years = 0
-
 while a <= b:
-    a *= 3
-    b *= 2
-    years += 1
-
-print( years)
+    a *= 3  
+    b *= 2  
+    years += 1  
+print(years)
