4b1fec0269c367f6afdd5c354cd51526a8aae860
Tsita-w <t687959@gmail.com>
2025-02-05 14:26:02 +0300

    Update Anton and Danik.py

--- Anton and Danik.py
+++ Anton and Danik.py
@@ -1,19 +1,14 @@
+n, h = map(int, input().split())  
+a = list(map(int, input().split()))  
 
-n = int(input("Enter the number of games: "))
-s = input("Enter the outcomes of the games: ")
 
-anton_wins = 0
-danik_wins = 0
+total_width = 0
 
-for game in s:
-    if game == 'A':
-        anton_wins += 1
-    elif game == 'D':
-        danik_wins += 1
+for height in a:
+    if height > h:
+        total_width += 2  
+    else:
+        total_width += 1  
 
-if anton_wins > danik_wins:
-    print("Anton")
-elif danik_wins > anton_wins:
-    print("Danik")
-else:
-    print("Friendship")
+print(total_width)
+
