bb6ec42fb1f2198881a15d3cca32ff0a70758c9c
Tsita-w <t687959@gmail.com>
2025-02-05 18:45:26 +0300

    Add files via upload

---
+++ Petya and Strings.py
@@ -0,0 +1,13 @@
+def compare_strings(s1, s2):
+    s1, s2 = s1.lower(), s2.lower()
+    if s1 < s2:
+        return -1
+    elif s1 > s2:
+        return 1
+    else:
+        return 0
+
+s1 = input().strip()
+s2 = input().strip()
+
+print(compare_strings(s1, s2))
